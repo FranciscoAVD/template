0d7f5eea1724153df54c6d7303b734766b6530a2
Francisco Victoriano <fvictoriano.dev@gmail.com>
2026-01-14 07:28:26 -0400

    webhook pseudo code

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -1,6 +1,21 @@
-import { apiResponse } from "@/lib/server-utils";
+import { apiResponse } from "@/lib/api-utils";
 import type { NextRequest } from "next/server";
 
 async function POST(req: NextRequest) {
+  /*
+- Verify Signature: Reject immediately if invalid (400).
+- Filter Events: Ignore irrelevant events (200).
+- Idempotency Check:
+	- Check Redis for Event ID.
+	- If "Processing" or "Completed," return 200.
+	- Else, set status to "Processing" with an expiry (e.g., 5 minutes).
+- Database Transaction:
+	- Update user records.
+	- If failure: Delete Redis key (or set to "Failed") and return 500.
+- Finalize Cache: Set Redis key to "Completed" with a
+	longer TTL.
+
+- Return 200.
+*/
   return apiResponse("SUCCESS_NO_CONTENT");
 }
--- src/features/payments/lib/constants.ts
+++ src/features/payments/lib/constants.ts
@@ -4,3 +4,7 @@ const TEST_PAYMENT_CARDS = [
   "6011_1111_1111_1117", // Discover 3-digit CVC
   "3782_822463_10005", // Amex 4-digit CVC
 ] as const;
+
+const ALLOWED_STRIPE_EVENTS = [] as const;
+
+export { ALLOWED_STRIPE_EVENTS, TEST_PAYMENT_CARDS };
